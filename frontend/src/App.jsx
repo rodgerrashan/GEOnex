e1dba8832a04c8c46c793895edbb443a16401d6b
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2025-02-20 01:31:20 +0530

    setup the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Devices from './pages/Devices';
+import Projects from './pages/Projects';
+import Settings from './pages/Settings';
+import Login from "./pages/Login";
+
 
 const App = () => {
   return (
-    <div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px=[9vw]">
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+
+        <Route path='/devices' element={<Devices />}/>
+
+        <Route path='/projects' element={<Projects />}/>
+
+        <Route path='/settings' element={<Settings />}/>
+
+        <Route path='/login' element={<Login />}/>
+
+      </Routes>
       
     </div>
   )
