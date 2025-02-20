0262e676366b3d14e72fe215792ab2e05eff5f9d
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2025-02-20 17:06:27 +0530

    add base and device icons

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,10 +2,14 @@ import home from './home.png'
 import settings from './setting.png'
 import devices from './location.png'
 import projects from './map.png'
+import base from './base.png'
+import device from './device.png'
 
 export const assets = {
     home,
     settings,
     devices,
-    projects
+    projects,
+    base,
+    device
 }
