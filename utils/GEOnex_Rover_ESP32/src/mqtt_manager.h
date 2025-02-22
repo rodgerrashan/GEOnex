8076a6d8a76e88fd9082c5202fb71e0f9620abb7
Samuditha Seneviratne <e20369@eng.pdn.ac.lk>
2025-02-23 00:02:15 +0530

    Adding Rover

---
+++ utils/GEOnex_Rover_ESP32/src/mqtt_manager.h
@@ -0,0 +1,11 @@
+#ifndef MQTT_MANAGER_H
+#define MQTT_MANAGER_H
+
+void connectMQTT();
+void publishGPSData(float latitude, float longitude, int satellites);
+
+bool mqttConnected();
+void mqttLoop();
+void mockPublishGPSData();
+
+#endif
