48201ead8080f649fa12bff49128b2c6bf2b4ff3
RodgerJay <124046523+rodgerrashan@users.noreply.github.com>
2025-02-23 12:04:15 +0530

    Add button manager and refactor GPS handling; remove unused pins header

--- utils/GEOnex_Rover_ESP32/src/gps_manager.h
+++ utils/GEOnex_Rover_ESP32/src/gps_manager.h
@@ -1,27 +1,10 @@
-#ifndef GNSS_ESP32_H
-#define GNSS_ESP32_H
+#ifndef GPS_MANAGER_H
+#define GPS_MANAGER_H
 
-#include <Arduino.h>
-#include <HardwareSerial.h>
 #include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
-class GPSModule
-{
-public:
-    GPSModule(int rxPin, int txPin, int baudRate);
-    void processGPSData();
-    bool hasNewLocation();
+void processGPS();
+void handleGPSLED(int sat);
 
-    double getLatitude();
-    double getLongitude();
-    double getAltitude();
-    double getSpeed();
-    int getSatellites();
-    String getDateTime();
-
-private:
-    HardwareSerial gpsSerial;
-    TinyGPSPlus gps;
-};
-
-#endif // GNSS_ESP32_H
+#endif 
