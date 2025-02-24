894d119c96ab6f6f54ee61a2bafd0dbaabfd914d
Samuditha Seneviratne <e20369@eng.pdn.ac.lk>
2025-02-24 10:52:36 +0530

    Base station project added

---
+++ utils/GEOnex_Base_ESP32/src/gnss_esp.cpp
@@ -0,0 +1,34 @@
+#include "gnss_esp.h"
+
+GPSModule::GPSModule(int rxPin, int txPin, int baudRate) : gpsSerial(2)
+{
+    gpsSerial.begin(baudRate, SERIAL_8N1, rxPin, txPin);
+}
+
+void GPSModule::processGPSData()
+{
+    while (gpsSerial.available())
+    {
+        gps.encode(gpsSerial.read());
+    }
+}
+
+bool GPSModule::hasNewLocation()
+{
+    return gps.location.isUpdated();
+}
+
+double GPSModule::getLatitude()
+{
+    return gps.location.lat();
+}
+
+double GPSModule::getLongitude()
+{
+    return gps.location.lng();
+}
+
+int GPSModule::getSatellites()
+{
+    return gps.satellites.value();
+}
