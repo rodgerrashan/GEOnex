daf23c5137e4725686ac18c690da81c3d560722a
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2025-05-04 12:46:40 +0530

    make projects page responsive

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../context/Context";
 import dayjs from "dayjs";
 import { assets } from "../assets/assets";
-import axios from 'axios';
+import axios from "axios";
 import { toast } from "react-toastify";
 
 const Projects = () => {
-  const { navigate, projects, getProjectsData, removeProject } = useContext(Context);
+  const { navigate, projects, getProjectsData, removeProject } =
+    useContext(Context);
 
   useEffect(() => {
     getProjectsData();
@@ -15,19 +16,27 @@ const Projects = () => {
   return (
     <div>
       <div
-        className="grid grid-cols-4 gap-4"
-        style={{ gridTemplateRows: "80px auto" }}
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4
+        md:grid-rows-[80px_auto]"
       >
-        <div className="col-span-4 flex items-center justify-between">
+        <div
+          className="col-span-1 md:col-span-2 lg:col-span-4 
+         flex items-center justify-between"
+        >
           {/* Left side: Title & Subtitle */}
           <div>
-            <h1 className="text-2xl font-semi-bold">Projects</h1>
-            <p className="text-xs mt-1">Manage your all projects</p>
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-semi-bold">
+              Projects
+            </h1>
+            <p className="text-sm md:text-base lg:text-lg mt-1">
+              Manage your all projects
+            </p>
           </div>
 
           {/* Right side: "Add New Project" Button */}
           <button
-            className="flex items-center gap-1 text-s px-4 py-2 bg-black text-white rounded-lg"
+            className="flex text-sm md:text-lg lg:text-xl
+            items-center gap-1 text-s px-4 py-2 bg-black text-white rounded-lg"
             onClick={() => {
               navigate("/newproject");
             }}
@@ -37,14 +46,17 @@ const Projects = () => {
           </button>
         </div>
 
-        <div className="col-span-3  bg-white p-4 rounded-lg">
+        <div
+          className="col-span-1 md:col-span-2 lg:col-span-3
+          bg-white p-4 rounded-lg"
+        >
           <div className="bg-white p-4 rounded-lg">
             <div className="overflow-x-auto">
               <table
                 className="w-full text-sm text-left border-separate border-spacing-y-2"
                 style={{ borderCollapse: "separate" }}
               >
-                <thead className="text-base text-gray-700 uppercase bg-gray-50">
+                <thead className="text-xs md:text-sm lg:text-base text-gray-700 uppercase bg-gray-50">
                   <tr>
                     <th scope="col" className="px-6 py-3">
                       Project
@@ -75,7 +87,7 @@ const Projects = () => {
                         key={index}
                         style={{ backgroundColor: "rgba(197,197,198,1)" }}
                         onClick={() => navigate(`/project/${project._id}`)}
-                        className="cursor-pointer"
+                        className="cursor-pointer text-xs md:text-sm lg:text-base"
                       >
                         <td className="px-6 py-4 rounded-l-lg">
                           {project.Name}
@@ -86,7 +98,7 @@ const Projects = () => {
                         <td className="px-6 py-4 rounded-r-lg">
                           <div className="flex items-center justify-between">
                             <span
-                              className={`px-1 py-1 text-sm font-semibold ${
+                              className={`px-1 py-1 text-xs md:text-sm lg:text-base font-semibold ${
                                 project.Status === "Active"
                                   ? "text-blue-700"
                                   : project.Status === "Pending"
@@ -97,9 +109,12 @@ const Projects = () => {
                               {project.Status}
                             </span>
                             <img
-                              src={assets.bin} 
+                              src={assets.bin}
                               alt="Delete Project"
-                              className="w-5 h-5 cursor-pointer"
+                              className="w-3 h-3       
+                                      sm:w-4 sm:h-4 
+                                      md:w-5 md:h-5
+                                      cursor-pointer"
                               onClick={(e) => {
                                 e.stopPropagation();
                                 removeProject(project._id);
