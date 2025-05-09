4d43246daa57f568da77bbcc0227527cabf7b812
rodgerrashan <rodrasjay@gmail.com>
2025-05-09 22:08:21 +0530

    API added: user can add a device

--- backend/src/services/auth-service/controllers/userController.js
+++ backend/src/services/auth-service/controllers/userController.js
@@ -25,4 +25,35 @@ const getUserData = async (req, res) => {
     }
 };
 
-module.exports = {getUserData}
+
+const Device = require('../../device-service/models/Device'); 
+
+const addDeviceToUser = async (req, res) => {
+    const userId = req.params.userId;
+    const { DeviceCode } = req.body;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const device = await Device.findOne({ DeviceCode: DeviceCode });
+        if (!device) {
+            return res.status(404).json({ message: 'Device not found' });
+        }
+
+
+        // Add device to user's connectedDevices
+        user.connectedDevices.push(device._id);
+        await user.save();
+
+        res.status(200).json({ message: 'Device added to user successfully', connectedDevices: user.connectedDevices });
+    } catch (error) {
+        console.error('Error assigning device to user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
+module.exports = {getUserData,addDeviceToUser}
--- backend/src/services/auth-service/routes/userRoutes.js
+++ backend/src/services/auth-service/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userAuth = require('../middleware/userAuth.js');
 const { getUserData } = require('../controllers/userController.js');
+const { addDeviceToUser } = require('../controllers/userController.js');
 
 const userRouter = express.Router();
 
-userRouter.get('/data',userAuth,getUserData)
+userRouter.get('/data',userAuth,getUserData);
+userRouter.post('/:userId/add-device',addDeviceToUser);
 
 module.exports = userRouter;
