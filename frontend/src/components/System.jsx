88dfe8d95f350b5078920f00b251c9945c172a0e
NisithaPadeniya <e20276@eng.pdn.ac.lk>
2025-05-15 00:16:01 +0530

    create setting page

---
+++ frontend/src/components/System.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const System = () => {
+  return (
+    <div>
+      <h2 className="font-semibold text-xl">System</h2>
+
+      {/* Distance Unit */}
+      <div className="flex items-center justify-between mt-4">
+        <span>Distance Unit</span>
+        <select className="text-blue-600 cursor-pointer outline-none px-2 py-1">
+          <option>CM</option>
+          <option>FEET</option>
+        </select>
+      </div>
+      <hr className="border-gray-300" />
+
+
+      {/* Coordinates Unit */}
+      <div className="flex items-center justify-between mt-4">
+        <span>Coordinates Unit</span>
+        <select
+          className="text-gray-800 cursor-pointer outline-none px-2 py-1"
+          disabled
+        >
+          <option>DEGREES</option>
+        </select>
+      </div>
+      <hr className="border-gray-300" />
+
+      {/* Base Station Coordinates */}
+      <div className="flex items-center justify-between mt-4">
+        <span>Base Station Coordinates</span>
+        <select className="text-blue-600 cursor-pointer outline-none rounded px-2 py-1">
+          <option>AUTO</option>
+          <option>MANUAL</option>
+        </select>
+      </div>
+      <hr className="border-gray-300" />
+
+      {/* Longitude and Latitude placeholders */}
+      <div className="flex gap-2 mt-6 mx-4">
+        <input
+          type="text"
+          placeholder="Longitude"
+          disabled
+          className="flex-1 bg-gray-200 rounded-full py-1 px-2 text-center text-gray-800 cursor-not-allowed"
+        />
+        <input
+          type="text"
+          placeholder="Latitude"
+          disabled
+          className="flex-1 bg-gray-200 rounded-full py-1 px-2 text-center text-gray-500 cursor-not-allowed"
+        />
+        <button
+          disabled
+          className="rounded-full bg-gray-200 w-7 h-7 flex items-center justify-center text-gray-500 cursor-not-allowed"
+        >
+          x
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default System;
