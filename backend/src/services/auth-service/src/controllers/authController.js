321e4b06394706b2159846dc2340e2e628251428
rodgerrashan <rodrasjay@gmail.com>
2025-06-03 23:27:42 +0530

    fix(auth-service): disable secure cookie settings for development environment

--- backend/src/services/auth-service/src/controllers/authController.js
+++ backend/src/services/auth-service/src/controllers/authController.js
@@ -66,8 +66,9 @@ const register = async(req,res)=>{
 
         res.cookie('token',token, {
             httpOnly: true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite: process.env.NODE_ENV==='production'?'none':'strict',
+            // secure:process.env.NODE_ENV==='production',
+            secure:false,
+            // sameSite: process.env.NODE_ENV==='production'?'none':'strict',
             maxAge: 7*24*60*60*1000
         })
 
@@ -120,6 +121,7 @@ const login = async(req,res)=>{
 
         res.cookie('token',token, {
             httpOnly: true,
+            secure:false,
             // secure:process.env.NODE_ENV==='production',
             // sameSite: process.env.NODE_ENV==='production'?'none':'strict',
             maxAge: 7*24*60*60*1000
@@ -138,6 +140,7 @@ const logout= async (req,res)=>{
     try {
         res.clearCookie('token',{
             httpOnly: true,
+            secure:false,
             // secure:process.env.NODE_ENV==='production',
             // sameSite: process.env.NODE_ENV==='production'?'none':'strict'
         })
